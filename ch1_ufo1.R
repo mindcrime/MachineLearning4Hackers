1f3192f19bbdb573ad0cea4e0533660d47009bb8
Phillip Rhodes <motley.crue.fan@gmail.com>
2015-09-09 22:24:20 -0400

    minor tweaks to ch1_ufo1.R

--- ch1_ufo1.R
+++ ch1_ufo1.R
@@ -8,9 +8,12 @@ library('ggplot2')
 
 ufo <- read.delim( "/home/prhodes/development/books/MachineLearning4Hackers/ML_for_Hackers/01-Introduction/data/ufo/ufo_awesome.tsv", sep="\t", stringsAsFactors=FALSE, header=FALSE, na.strings="" )
 
+names(ufo) <- c( "DateOccurred", "DateReported", "Location", "ShortDescription", "Duration", "LongDescription")
+
+ufo$DateOccurred<- as.Date(ufo$DateOccurred, format="%Y%m%d")
+
 head(ufo)
 
-writeLines( "\n\n\n\n")
-
-tail(ufo)
+# writeLines( "\n\n\n\n")
+# tail(ufo)
 
